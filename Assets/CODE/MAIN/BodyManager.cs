e1a77f73e631d649df8cebf9ab320bf347ca4990
cmspice <fish@robotbear.org>
2012-11-14 21:06:19 -0800

    fixes in body manager

--- Assets/CODE/MAIN/BodyManager.cs
+++ Assets/CODE/MAIN/BodyManager.cs
@@ -14,6 +14,9 @@ public class BodyManager : FakeMonoBehaviour {
 	public GameObject create_object(ZigJointId aId, Texture2D aTex)
 	{
 		GameObject parent = new GameObject("genParent"+aId.ToString());
+		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphere.transform.localScale = Vector3.one * 0.2f;
+		sphere.transform.parent = parent.transform;
 		GameObject kid = GameObject.CreatePrimitive(PrimitiveType.Plane);
         kid.transform.rotation =  Quaternion.AngleAxis(90, Vector3.right) * kid.transform.rotation;
         kid.transform.localScale = new Vector3(convert_units(aTex.width) / 10.0f, 1, convert_units(aTex.height) / 10.0f);
@@ -22,13 +25,6 @@ public class BodyManager : FakeMonoBehaviour {
         if (aId == ZigJointId.LeftShoulder || aId == ZigJointId.RightShoulder || aId == ZigJointId.LeftHip || aId == ZigJointId.RightHip || 
             aId == ZigJointId.LeftElbow || aId == ZigJointId.RightElbow || aId == ZigJointId.LeftKnee || aId == ZigJointId.RightKnee || aId == ZigJointId.Waist)
             kid.transform.position = new Vector3(0, convert_units(-aTex.height / 2.0f * 0.8f), 0);
-
-        /*GameObject tempParent = new GameObject("genTempParent");
-        kid.transform.parent = tempParent.transform;
-        if (aId == ZigJointId.LeftShoulder || aId == ZigJointId.RightShoulder || aId == ZigJointId.LeftHip || aId == ZigJointId.RightHip || aId == ZigJointId.Neck || aId == ZigJointId.Waist)
-            tempParent.transform.rotation = Quaternion.AngleAxis(-90, Vector3.forward) * tempParent.transform.rotation;
-		kid.transform.parent = parent.transform;
-        GameObject.Destroy(tempParent);*/
 
         kid.transform.parent = parent.transform;
 
@@ -42,18 +38,20 @@ public class BodyManager : FakeMonoBehaviour {
 			if(A == ZigJointId.Torso)
 				return new Vector3(0,convert_units(aBTex.height/2.0*0.8),0);
 			else if(A == ZigJointId.LeftHip)
-				return new Vector3(convert_units(-aBTex.width/2.0*0.8),convert_units(-aBTex.height/2.0*0.8),0);
-			else if(A == ZigJointId.RightHip)
 				return new Vector3(convert_units(aBTex.width/2.0*0.8),convert_units(-aBTex.height/2.0*0.8),0);
+			else if(A == ZigJointId.RightHip)
+				return new Vector3(convert_units(-aBTex.width/2.0*0.8),convert_units(-aBTex.height/2.0*0.8),0);
 		}
 		if(B == ZigJointId.Torso)
 		{
-			if(A == ZigJointId.Neck)
+			if(A == ZigJointId.Waist)
+				return Vector3.zero;
+			else if(A == ZigJointId.Neck)
 				return new Vector3(0,convert_units(aBTex.height/2.0*0.8),0);
 			else if(A == ZigJointId.LeftShoulder)
-				return new Vector3(convert_units(-aBTex.width/2.0*0.8),convert_units(aBTex.height/2.0*0.8),0);
-			else if(A == ZigJointId.RightShoulder)
 				return new Vector3(convert_units(aBTex.width/2.0*0.8),convert_units(aBTex.height/2.0*0.8),0);
+			else if(A == ZigJointId.RightShoulder)
+				return new Vector3(convert_units(-aBTex.width/2.0*0.8),convert_units(aBTex.height/2.0*0.8),0);
 		}
 		else if(A == ZigJointId.Torso)
 		{
@@ -61,7 +59,7 @@ public class BodyManager : FakeMonoBehaviour {
 		}
 		else if(A != ZigJointId.None)
 		{
-			return new Vector3(0,convert_units(aBTex.height/2.0 - aBTex.width*0.1),0);
+			return new Vector3(0,-convert_units(aBTex.height/2.0 - aBTex.width*0.1),0);
 		}
 		else
 		{
@@ -131,29 +129,51 @@ public class BodyManager : FakeMonoBehaviour {
 		GameObject leftLowerLeg = create_object(ZigJointId.LeftKnee,aChar.leftLowerLeg);
 		GameObject rightLowerLeg = create_object(ZigJointId.RightKnee,aChar.rightLowerLeg);
 		
-        //TODO connect waist somewhere??
+		
 		torso.transform.position = get_connection_point(ZigJointId.Torso,ZigJointId.Waist,aChar.waist);
-		//torso.transform.parent = waist.transform;//TODO DELEE we don't need this anymore
-		head.transform.position = get_connection_point(ZigJointId.Neck,ZigJointId.Torso,aChar.torso);
+		waist.transform.position = get_connection_point(ZigJointId.Waist,ZigJointId.Torso,aChar.torso);
+		
 		head.transform.parent = torso.transform;
-		leftUpperArm.transform.position = get_connection_point(ZigJointId.LeftShoulder,ZigJointId.Torso,aChar.torso);
+		head.transform.position = head.transform.parent.position + get_connection_point(ZigJointId.Neck,ZigJointId.Torso,aChar.torso);
+		
 		leftUpperArm.transform.parent = torso.transform;
-		rightUpperArm.transform.position = get_connection_point(ZigJointId.RightShoulder,ZigJointId.Torso,aChar.torso);
+		leftUpperArm.transform.localPosition = leftUpperArm.transform.parent.global_scale().component_inverse().component_multiply(get_connection_point(ZigJointId.LeftShoulder,ZigJointId.Torso,aChar.torso));
+		//leftUpperArm.transform.position = leftUpperArm.transform.parent.position + get_connection_point(ZigJointId.LeftShoulder,ZigJointId.Torso,aChar.torso);
+		
 		rightUpperArm.transform.parent = torso.transform;
-		leftLowerArm.transform.position = get_connection_point(ZigJointId.LeftElbow,ZigJointId.LeftShoulder,aChar.leftUpperArm);
+		rightUpperArm.transform.position = rightUpperArm.transform.parent.position + get_connection_point(ZigJointId.RightShoulder,ZigJointId.Torso,aChar.torso);
+		
 		leftLowerArm.transform.parent = leftUpperArm.transform;
-		rightLowerArm.transform.position = get_connection_point(ZigJointId.RightElbow,ZigJointId.RightShoulder,aChar.rightUpperArm);
+		//leftLowerArm.transform.localPosition = leftLowerArm.transform.parent.global_scale().component_inverse().component_multiply(get_connection_point(ZigJointId.LeftElbow,ZigJointId.LeftShoulder,aChar.leftUpperArm));
+		leftLowerArm.transform.position = leftLowerArm.transform.parent.position + get_connection_point(ZigJointId.LeftElbow,ZigJointId.LeftShoulder,aChar.leftUpperArm);
+		
 		rightLowerArm.transform.parent = rightUpperArm.transform;
+		rightLowerArm.transform.position = rightLowerArm.transform.parent.position + get_connection_point(ZigJointId.RightElbow,ZigJointId.RightShoulder,aChar.rightUpperArm);
 		
-		leftUpperLeg.transform.position = get_connection_point(ZigJointId.LeftHip,ZigJointId.Waist,aChar.torso);
 		leftUpperLeg.transform.parent = waist.transform;
-		rightUpperLeg.transform.position = get_connection_point(ZigJointId.RightHip,ZigJointId.Waist,aChar.torso);
-		rightUpperLeg.transform.parent = waist.transform;
-		leftLowerLeg.transform.position = get_connection_point(ZigJointId.LeftElbow,ZigJointId.LeftHip,aChar.leftUpperLeg);
-		leftLowerLeg.transform.parent = leftUpperLeg.transform;
-		rightLowerLeg.transform.position = get_connection_point(ZigJointId.RightElbow,ZigJointId.RightHip,aChar.rightUpperLeg);
-		rightLowerLeg.transform.parent = rightUpperLeg.transform;
+		leftUpperLeg.transform.position = leftUpperLeg.transform.parent.position + get_connection_point(ZigJointId.LeftHip,ZigJointId.Waist,aChar.torso);
 		
+		rightUpperLeg.transform.parent = waist.transform;
+		rightUpperLeg.transform.position = rightUpperLeg.transform.parent.position + get_connection_point(ZigJointId.RightHip,ZigJointId.Waist,aChar.torso);
+		
+		leftLowerLeg.transform.parent = leftUpperLeg.transform;
+		leftLowerLeg.transform.position = leftLowerLeg.transform.parent.position + get_connection_point(ZigJointId.LeftElbow,ZigJointId.LeftHip,aChar.leftUpperLeg);
+		
+		rightLowerLeg.transform.parent = rightUpperLeg.transform;
+		rightLowerLeg.transform.position = rightLowerLeg.transform.parent.position + get_connection_point(ZigJointId.RightElbow,ZigJointId.RightHip,aChar.rightUpperLeg);
+		
+		
+		List<GameObject> rotateMe;
+		rotateMe.Add(leftUpperArm);
+		rotateMe.Add(rightUpperArm);
+		//rotateMe.Add(leftUpperArm);
+		//rotateMe.Add(leftUpperArm);
+        GameObject tempParent = new GameObject("genTempParent");
+        kid.transform.parent = tempParent.transform;
+        if (aId == ZigJointId.LeftShoulder || aId == ZigJointId.RightShoulder || aId == ZigJointId.LeftHip || aId == ZigJointId.RightHip || aId == ZigJointId.Neck || aId == ZigJointId.Waist)
+            tempParent.transform.rotation = Quaternion.AngleAxis(-90, Vector3.forward) * tempParent.transform.rotation;
+		kid.transform.parent = parent.transform;
+        GameObject.Destroy(tempParent);
 		
 	}
 }
---
+++ Assets/CODE/UTILITIES/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods  {
+	public static Vector3 component_multiply(this Vector3 aVec, Vector3 scaleVec)
+    {
+        return new Vector3(aVec.x*scaleVec.x,aVec.y*scaleVec.y,aVec.z*scaleVec.z);
+    }
+	public static Vector3 component_inverse(this Vector3 aVec)
+    {
+        return new Vector3(1/aVec.x, 1/aVec.y, 1/aVec.z);
+    }
+	public static Vector3 global_scale(this Transform aTrans)
+	{
+		if(aTrans.parent == null)
+			return aTrans.localScale;
+		return aTrans.localScale.component_multiply(global_scale(aTrans.parent));
+	}
+}
