2ffbdc5a3bc6f4f4f2d0076b5903df41de7fd0cf
peters pc laptop <fish@robotbear.org>
2012-12-06 16:21:22 -0800

    added mini characetr and fixejd 34-2

---
+++ Assets/CODE/UI/FlatElementText.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlatElementText : FlatElementBase
+{
+    TextMesh mMesh;
+    Renderer mRenderer;
+
+    public int Size
+    {
+        get
+        {
+            return mMesh.fontSize;
+        }
+        set
+        {
+            mMesh.fontSize = value;
+        }
+    }
+
+    public string Text
+    {
+        get { return mMesh.text; }
+        set { mMesh.text = value; }
+    }
+            
+    public FlatElementText(Font aFont, int aSize, string aText, int aDepth)
+    {
+        PrimaryGameObject = new GameObject("genTextElement");
+        mRenderer = PrimaryGameObject.AddComponent<MeshRenderer>();
+        //TODO set material
+        mMesh = PrimaryGameObject.AddComponent<TextMesh>();
+        mMesh.font = aFont;
+        Size = aSize;
+        Text = aText;
+        Depth = aDepth;
+    }
+}
