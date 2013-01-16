341047be9dbc3e995a924e5fe373c3fc7302257d
peters pc laptop <fish@robotbear.org>
2013-01-15 17:37:27 -0800

    added grading function stubs

--- Assets/CODE/PD/PDPlayerStats.cs
+++ Assets/CODE/PD/PDPlayerStats.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class PDPlayerstats 
 {
@@ -17,5 +18,33 @@ public class PDPlayerstats
     public Dictionary<PDStats.Stats, float> Values { get { return mValues; } }
 
 
+
+    
+
+    public float grade_absolute(PDCharacterStats aStat)
+    {
+        //TODO 
+        return 0;
+    }
+    public int difficulty_absolute(PDCharacterStats aStat)
+    {
+        //TODO 
+        return 0;
+    }
+    public int[] difficulty_absolute(PDCharacterStats[] aStats)
+    {
+        int[] r = new int[4] { 0, 1, 2, 3 };
+        for (int i = 0; i < 4; i++)
+            r[i] = difficulty_absolute(aStats[i]);
+        return r;
+    }
+    public int[] difficulty_relative(PDCharacterStats[] aStats)
+    {
+        int[] r = new int[4]{0,1,2,3};
+        float[] orig = new float[4];
+        for (int i = 0; i < 4; i++)
+            orig[i] = grade_absolute(aStats[i]);
+        return r.OrderBy(x => orig[x]).ToArray(); //TODO is this correct?
+    }
 }
     
