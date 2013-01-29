6ca9e7179b5705edd009747c57ec42b23975be65
cmspice <fish@robotbear.org>
2013-01-29 10:16:10 -0800

    updtade PDCHarceters.cs

--- Assets/CODE/PD/PDCharacters.cs
+++ Assets/CODE/PD/PDCharacters.cs
@@ -11,13 +11,13 @@ public class PDCharacters
         },   
         new PDCharacterStats()      //05-1
         {
-            EDUCATION = 1,
-            EXPRESSION = 0,
-            FAMILY = 1,
-            HEALTH = 0,
+            EDUCATION = 0,
+            EXPRESSION = 5,
+            FAMILY = 0,
+            HEALTH = -1,
             MONEY = 0,
-            RESPECT = 1,
-            ROMANCE = 0,
+            RESPECT = 0,
+            ROMANCE = 5,
             WISDOM = 0,
 
             Title = "Princess"
@@ -25,63 +25,63 @@ public class PDCharacters
         new PDCharacterStats()      //05-2
         {
             EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = -1,
+            EXPRESSION = 100,
+            FAMILY = -5,
+            HEALTH = -100,
             MONEY = 0,
-            RESPECT = 0,
-            ROMANCE = 4,
-            WISDOM = 0,
+            RESPECT = 5,
+            ROMANCE = 0,
+            WISDOM = -5,
 
             Title = "Child Star"
         },
         new PDCharacterStats()      //05-3
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 2,
-            HEALTH = 0,
+            EDUCATION = -100,
+            EXPRESSION = 5,
+            FAMILY = -5,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 100,
 
             Title = "Street Kid"
         },
         new PDCharacterStats()      //05-4
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = 5,
+            EXPRESSION = 5,
+            FAMILY = 100,
+            HEALTH = 5,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 5,
 
             Title = "1 of 12"
         },
         new PDCharacterStats()      //16-1
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = -100,
+            EXPRESSION = -5,
+            FAMILY = 5,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = -5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 100,
 
             Title = "Responsible Sister"
         },
         new PDCharacterStats()      //16-2
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
+            EDUCATION = 100,
+            EXPRESSION = 5,
+            FAMILY = -5,
             HEALTH = 0,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
             WISDOM = 0,
 
@@ -90,11 +90,11 @@ public class PDCharacters
         new PDCharacterStats()      //16-3
         {
             EDUCATION = 0,
-            EXPRESSION = 0,
+            EXPRESSION = 5,
             FAMILY = 0,
             HEALTH = 0,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
             WISDOM = 0,
 
@@ -102,12 +102,12 @@ public class PDCharacters
         },
         new PDCharacterStats()      //16-4
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
+            EDUCATION = -5,
+            EXPRESSION = 100,
             FAMILY = 0,
-            HEALTH = 0,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
             WISDOM = 0,
 
@@ -115,38 +115,38 @@ public class PDCharacters
         },
         new PDCharacterStats()      //27-1
         {
-            EDUCATION = 0,
+            EDUCATION = 5,
             EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            FAMILY = 100,
+            HEALTH = 5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 5,
 
             Title = "Mother"
         },
         new PDCharacterStats()      //27-2
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = 100,
+            EXPRESSION = 5,
+            FAMILY = -100,
+            HEALTH = -100,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = -5,
 
             Title = "Drug User"
         },
         new PDCharacterStats()      //27-3
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
+            EDUCATION = -100,
+            EXPRESSION = 5,
             FAMILY = 0,
-            HEALTH = 0,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = -5,
             ROMANCE = 0,
             WISDOM = 0,
 
@@ -154,10 +154,10 @@ public class PDCharacters
         },
         new PDCharacterStats()      //27-4
         {
-            EDUCATION = 0,
+            EDUCATION = -100,
             EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            FAMILY = -5,
+            HEALTH = -5,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
@@ -167,10 +167,10 @@ public class PDCharacters
         },
         new PDCharacterStats()      //34-1
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = -5,
+            EXPRESSION = -5,
+            FAMILY = 100,
+            HEALTH = 5,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
@@ -180,64 +180,64 @@ public class PDCharacters
         },
         new PDCharacterStats()      //34-2
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = 100,
+            EXPRESSION = 5,
+            FAMILY = -100,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 100,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = -100,
 
             Title = "MIT Professor giving a TED talk"
         },
         new PDCharacterStats()      //34-3
         {
             EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EXPRESSION = -100,
+            FAMILY = -5,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 100,
 
             Title = "Sad Person"
         },
         new PDCharacterStats()      //34-4
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = -5,
+            EXPRESSION = 100,
+            FAMILY = 100,
+            HEALTH = 5,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 5,
 
             Title = "Foster Parent in Costa Rica"
         },
         new PDCharacterStats()      //45-1
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
+            EDUCATION = 5,
+            EXPRESSION = 5,
+            FAMILY = 5,
             HEALTH = 0,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = -5,
 
             Title = "Rich Married Woman"
         },
         new PDCharacterStats()      //45-2
         {
             EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EXPRESSION = -5,
+            FAMILY = -5,
+            HEALTH = -100,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = -5,
             ROMANCE = 0,
             WISDOM = 0,
 
@@ -245,23 +245,23 @@ public class PDCharacters
         },
         new PDCharacterStats()      //45-3
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
+            EDUCATION = 5,
+            EXPRESSION = 100,
+            FAMILY = -5,
             HEALTH = 0,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 100,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 5,
 
             Title = "Famosu Actress"
         },
         new PDCharacterStats()      //45-4
         {
             EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EXPRESSION = 100,
+            FAMILY = 5,
+            HEALTH = 5,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
@@ -271,10 +271,10 @@ public class PDCharacters
         },
         new PDCharacterStats()      //60-1
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
+            EDUCATION = -5,
+            EXPRESSION = -5,
             FAMILY = 0,
-            HEALTH = 0,
+            HEALTH = -5,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
@@ -284,48 +284,48 @@ public class PDCharacters
         },
         new PDCharacterStats()      //60-2
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = 100,
+            EXPRESSION = 5,
+            FAMILY = -100,
+            HEALTH = -5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 100,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 5,
 
             Title = "Foreign Minister"
         },
         new PDCharacterStats()      //60-3
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
+            EDUCATION = -5,
+            EXPRESSION = 5,
+            FAMILY = 5,
             HEALTH = 0,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 5,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = -5,
 
             Title = "Charity Speaker"
         },
         new PDCharacterStats()      //60-4
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
+            EDUCATION = 5,
+            EXPRESSION = 5,
+            FAMILY = 5,
             HEALTH = 0,
             MONEY = 0,
             RESPECT = 0,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 5,
 
             Title = "Intellectual"
         },
         new PDCharacterStats()      //80-1
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
+            EDUCATION = -5,
+            EXPRESSION = 5,
+            FAMILY = 5,
             HEALTH = 0,
             MONEY = 0,
             RESPECT = 0,
@@ -337,24 +337,24 @@ public class PDCharacters
         new PDCharacterStats()      //80-2
         {
             EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EXPRESSION = 100,
+            FAMILY = -100,
+            HEALTH = -100,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = -100,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = -100,
 
             Title = "Crazy Cat Lady"
         },
         new PDCharacterStats()      //80-3
         {
-            EDUCATION = 0,
+            EDUCATION = -100,
             EXPRESSION = 0,
-            FAMILY = 0,
+            FAMILY = 5,
             HEALTH = 0,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = -5,
             ROMANCE = 0,
             WISDOM = 0,
 
@@ -362,14 +362,14 @@ public class PDCharacters
         },
         new PDCharacterStats()      //80-4
         {
-            EDUCATION = 0,
-            EXPRESSION = 0,
-            FAMILY = 0,
-            HEALTH = 0,
+            EDUCATION = 5,
+            EXPRESSION = 5,
+            FAMILY = 100,
+            HEALTH = 5,
             MONEY = 0,
-            RESPECT = 0,
+            RESPECT = 100,
             ROMANCE = 0,
-            WISDOM = 0,
+            WISDOM = 100,
 
             Title = "Wise Grandma"
         },
