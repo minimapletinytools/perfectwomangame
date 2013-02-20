0e3773fd02a0f94bdd0f662bff31d25b291a93f2
peters pc laptop <fish@robotbear.org>
2013-02-19 16:17:53 -0800

    added stub for graphelement

---
+++ Assets/CODE/UI/FlatGraphElement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlatGraphElement : FlatElementImage {
+    Texture2D mGraphTexture;
+
+    public FlatGraphElement(int width, int height, int aDepth):base(null,aDepth)
+    {
+        mGraphTexture = new Texture2D(width,height);
+        mImage.set_new_texture(mGraphTexture);
+    }
+
+    public void draw_point(int x, int y, Color aColor)
+    {
+        //TODO
+    }
+
+    
+    public ImageGameObjectUtility mImage;
+}
