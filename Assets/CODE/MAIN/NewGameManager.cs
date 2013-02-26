bbf5547802d818ded7e7149ad96fd917e0c147ac
peters pc laptop <fish@robotbear.org>
2013-02-26 12:41:09 -0800

    added new game manager stub~

---
+++ Assets/CODE/MAIN/NewGameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class NewGameManager : FakeMonoBehaviour
+{
+    public NewGameManager(ManagerManager aManager)
+        : base(aManager) 
+    {
+    }
+
+    
+}
