dab6eb5dfc9fbbfd34026a424e653a48bf4776a7
cmspice <fish@robotbear.org>
2013-03-07 15:17:17 -0500

    added some stubs into asset bundle manager and loader

--- Assets/CODE/NEWGAME/AssetBundleManager.cs
+++ Assets/CODE/NEWGAME/AssetBundleManager.cs
@@ -10,11 +10,40 @@ public class AssetBundleManager {
 	
 	//load regular poses on request and distribute as appropriate
 	
-	public void load_poses()
-	{
-	}
+	
+	
+	//mini bundle related
 	
 	public void load_mini_characters()
 	{
+	
+	
+    }
+
+    //scene bundle related
+	AssetBundle mLastCharacterBundle = null;
+    public void scene_loaded_callback(AssetBundle aBundle, string aBundleName)
+    {
+		if(mLastCharacterBundle != null) //we are loading a new bundle so I guess we don't need the old bundle anymore
+			mLastCharacterBundle.Unload(true);
+	
+        //Debug.Log("loading character in CharacterLoader " + aBundleName);
+		mLastCharacterBundle = aBundle;
+        CharacterLoader loader = new CharacterLoader();
+        loader.complete_load_character(aBundle,aBundleName);
+	
+		//TODO
 	}
+	
+	
+	public void load_poses()
+	{
+		//TODO
+	}
+	public void pose_bundle_loaded_callback(AssetBundle aBundle)
+    {
+		//TODO
+        
+        aBundle.Unload(true); //don't need this anymore I don't ithnk...
+    }
 }
