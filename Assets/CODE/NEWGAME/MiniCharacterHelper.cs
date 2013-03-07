d4954a23a490846010940f4a4006a633dfb952e2
cmspice <fish@robotbear.org>
2013-03-07 15:16:47 -0500

    added mini character helper for eventual use.

---
+++ Assets/CODE/NEWGAME/MiniCharacterHelper.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class MiniCharacterHelper 
+{
+	public MiniCharacterHelper()
+	{
+	}
+	
+	
+	FlatBodyObject mBody;
+	public void load_character(AssetBundle aBundle, string aName)
+	{
+		CharacterLoader loader = new CharacterLoader();
+		loader.complete_load_character(aBundle,aName);
+		foreach(FlatBodyObject e in mBody.load_sequential(loader.Images,loader.Sizes))
+			;
+		
+		//TODO position mBody;
+	}
+	
+	public void position_character(FlatCameraManager aCam, Vector3 aPosition)
+	{
+		mBody.SoftPosition = aCam.Center + aPosition;
+	}
+}
