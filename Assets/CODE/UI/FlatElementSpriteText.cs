a21fcdd2b0dbee099200724927aaff1acb7fc325
cmspice <fish@robotbear.org>
2013-03-08 13:17:49 -0500

    added font scaling

--- Assets/CODE/UI/FlatElementSpriteText.cs
+++ Assets/CODE/UI/FlatElementSpriteText.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 
 public class FlatElementSpriteChar : FlatElementImage {
 	static string sWordOrder = "0123456789abcdefghijklmnopqrstuvwxyz!?[]{}()\\/+-*\" ,.:;=%<>|'^#_";
+	
+	Vector2 mDim;
+	public Vector2 Dimensions 
+	{
+		get{
+			return new Vector2(mDim.x*SoftScale.x,mDim.y*SoftScale.y);
+		}
+		private set
+		{
+			mDim = value;
+		}
+	}
     public FlatElementSpriteChar(Texture2D aTex, int pixelWidth, char aChar, int aDepth):base(aTex,aDepth)
     {
 		int index = sWordOrder.IndexOf(aChar);
@@ -11,14 +24,41 @@ public class FlatElementSpriteChar : FlatElementImage {
 		//mImage.ParentObject.GetComponentInChildren<Renderer>().material.SetTextureScale("_MainTex", new Vector2(pixelWidth / (float) aTex.width,1));
         mImage.TextureScale = new Vector2(pixelWidth / (float) aTex.width,1);
 		mImage.TextureOffset = new Vector2((index*pixelWidth)/(float)aTex.width,0);
-		
+		Dimensions = new Vector2(pixelWidth+1,aTex.height+1);
     }
-
 }
 public class FlatElementSpriteText : FlatElementMultiBase {
 	bool mHasWord = false;
 	Texture2D mTex = null;
 	int mCharacterWidth;
+	public float mfontScale = 1;
+	//float mMaxRowWidth = 200;
+	
+	public override Vector3 SoftScale
+    {
+        get { return base.SoftScale; }
+        set 
+        {
+            base.SoftScale = value;
+			update_font_size();
+        }
+    }
+	void update_font_size()
+	{
+		base.SoftScale = SoftScale;
+		//lame version
+		float netWidth = mElements.Sum(x => ((FlatElementSpriteChar)x.Element).Dimensions.x);
+		float curWidth = 0;
+		for(int i = 0; i < mElements.Count; i++)
+		{
+			var e = mElements[i];
+			var f = e.Element as FlatElementSpriteChar;
+			//f.SoftScale = new Vector3(FontScale,FontScale,FontScale);
+			e.Position = new Vector3(netWidth/2 - curWidth,0,0);
+			curWidth += f.Dimensions.x;
+		}
+		SoftPosition = SoftPosition; //this forces an update on position
+	}
 	public FlatElementSpriteText(Texture2D aTex, int pixelWidth, string aMsg, int aDepth)
 	{
 		mTex = aTex;
@@ -29,10 +69,6 @@ public class FlatElementSpriteText : FlatElementMultiBase {
         PrimaryGameObject = create_primary_from_elements();
         SoftInterpolation = SoftInterpolation;
 	
-	}
-	Vector3 get_offset()
-	{
-		return new Vector3(-mCharacterWidth * (4 + mElements.Count),0,0); //TODO wrapping
 	}
 	void create_filler()
 	{
@@ -45,8 +81,8 @@ public class FlatElementSpriteText : FlatElementMultiBase {
 		if(!mHasWord)
 			destroy();
 		mHasWord = true;
-		
-		mElements.Add(new FlatElementMultiBase.ElementOffset(new FlatElementSpriteChar(mTex,mCharacterWidth,aWord,Depth),get_offset()));
+		mElements.Add(new FlatElementMultiBase.ElementOffset(new FlatElementSpriteChar(mTex,mCharacterWidth,aWord,Depth),Vector3.zero));
+		update_font_size();
 	}
 	void add_string(string aStr)
 	{	
