ebc2f0ff8ddf231dd4afb8a96bb0eb4945768363
cmspice <fish@robotbear.org>
2013-03-13 21:48:38 -0400

    newChoiceObject now no longer needs body to be initialized. Can also swap bodies now.

--- Assets/CODE/NEWGAME/NEWUI/NewChoiceObject.cs
+++ Assets/CODE/NEWGAME/NEWUI/NewChoiceObject.cs
@@ -5,13 +5,17 @@ public class NewChoiceObject : FlatElementMultiBase {
     public FlatElementImage mSquare;
 	public FlatElementSpriteText mText;
 	public DifficultyObject mPerfect;
-    public FlatBodyObject mBody = null;
+	FlatElementMultiBase.ElementOffset mBodyElementOffset = null;
     public MeterImageObject mMeter = null;
 
     float mSide = 45;
     float mRightBot = -80;
-
-    public NewChoiceObject(CharacterLoader aActualChar, int aDepth)
+	
+	public NewChoiceObject(int aDepth)
+	{
+		initialize(null, aDepth);
+	}
+    void initialize(CharacterLoader aActualChar, int aDepth)
     {
 		//TODO finish and reposition everything
 		//mSquare = new FlatElementImage(, aDepth);
@@ -19,22 +23,38 @@ public class NewChoiceObject : FlatElementMultiBase {
         mPerfect = new DifficultyObject(ManagerManager.Manager.mMenuReferences.perfectnessStar, aDepth);
         //mMeter = new MeterImageObject(, MeterImageObject.FillStyle.DU, aDepth + 1);
         mMeter.Percentage = 0.0f;
-        mBody = new FlatBodyObject(aActualChar, aDepth + 2);
+        
         //mBody.set_target_pose(aPose);
 		mElements.Add(new FlatElementMultiBase.ElementOffset(mSquare, new Vector3(0, 0, 0)));
 		mElements.Add(new FlatElementMultiBase.ElementOffset(mText, new Vector3(0, 0, 0)));
 		mElements.Add(new FlatElementMultiBase.ElementOffset(mPerfect, new Vector3(0,0,0)));
         mElements.Add(new FlatElementMultiBase.ElementOffset(mMeter, new Vector3(0,0,0)));
-        mElements.Add(new FlatElementMultiBase.ElementOffset(mBody, new Vector3(0,0,0)));
+        
+		if(aActualChar != null)
+			set_actual_character(aActualChar);
 
         PrimaryGameObject = create_primary_from_elements();
 		Depth = aDepth;
     }
+	
+	
+	public void set_actual_character(CharacterLoader aActualChar)
+	{ 
+		//remove the old one
+		if(mBodyElementOffset != null)
+		{
+			mElements.Remove(mBodyElementOffset);
+			mBodyElementOffset.Element.destroy();
+		}
+		//add the new one
+		mBodyElementOffset = new FlatElementMultiBase.ElementOffset(new FlatBodyObject(aActualChar, Depth + 2), new Vector3(0,0,0));
+		mElements.Add(mBodyElementOffset);
+	}
 
     public void set_pose(ProGrading.Pose aPose)
     {
-        if (mBody != null)
-            mBody.set_target_pose(aPose);
+        if (mBodyElementOffset != null)
+            (mBodyElementOffset.Element as FlatBodyObject).set_target_pose(aPose);
     }
 
     public void set_perfectness(int perfectness)
