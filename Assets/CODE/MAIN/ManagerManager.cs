3de77c74a982a96f9bde455dfe8f0b318b409bff
cmspice <fish@robotbear.org>
2013-04-18 17:34:19 -0700

    added some dubg stuff into managermanager

--- Assets/CODE/MAIN/ManagerManager.cs
+++ Assets/CODE/MAIN/ManagerManager.cs
@@ -209,4 +209,12 @@ public class ManagerManager : MonoBehaviour{
         }
     }
 	
+	public string mDebugString = "";
+	void OnGUI()
+    {
+        GUI.depth = int.MinValue;
+        //GUI.Box(new Rect(0, Screen.height-50, 200, 50), "poo");
+        //GUI.Box(new Rect(20, 20, 80,30), mDebugString);
+    }
+	
 }
