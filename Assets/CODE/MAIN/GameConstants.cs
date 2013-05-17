f74b6103dc7fe82e6687e99996c499a73f5501ad
cmspice <fish@robotbear.org>
2013-05-17 12:47:53 -0700

    added constants

---
+++ Assets/CODE/MAIN/GameConstants.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public static class GameConstants
+{
+	public static Color[] IconDifficultyColors { get { return new Color[]{new Color(0,0.8f,0,1), new Color(0.8f,0.8f,0,1), new Color(0.9f,0.4f,0,1), new Color(0.8f,0,0,1)};}}
+	public static Color[] IconDifficultyColorsOverTwo { get{ return IconDifficultyColors.Select(e=> e/2f).ToArray();}}
+	
+	
+	public static Color PopupTextColor { get {return new Color(0,0,0); } } //TODO
+	public static Color ChoosingTextColor { get {return new Color(0,0,0); } } //TODO
+	
+}
