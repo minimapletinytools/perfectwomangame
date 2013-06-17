d120fb0e4a169789f65e6198630070dc6ea71637
cmspice <fish@robotbear.org>
2013-06-17 13:03:13 -0700

    cleaned up setupforrecording. Need to update it for ngm.

--- Assets/CODE/BEHAVIOUR/SetupForRecordingBehaviour.cs
+++ Assets/CODE/BEHAVIOUR/SetupForRecordingBehaviour.cs
@@ -2,45 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class SetupForRecordingBehaviour : MonoBehaviour {
-
-    public string prefix = "char";
-    public bool record_manual = true;
-    public bool record_kinect = true;
-
-    bool done = false;
-
-    //TODO use my new hack for loading scenes without all the other nonsense
-	void Start () {
-        ManagerManager.Manager.mRecordMode = true;
-        ManagerManager.Manager.mEventManager.character_changed_event += character_changed_listener;
-
-        ManagerManager.sTakeManual = record_manual;
-        ManagerManager.sTakeKinect = record_kinect;
-        ManagerManager.sScreenShotPrefix = prefix;
-	}
-	
-	void Update()
-	{
-		//hack to disable glow
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-		ManagerManager.Manager.mCameraManager.set_camera_effects(0);
-	}
-
-
-    public void character_changed_listener(CharacterLoader aCharacter)
-    {
-        if (!done)
-        {
-            ManagerManager.Manager.mGameManager.hack_choice(0, 9999999);
-            done = true;
-        }
-    }
+	//TODO
 	
 }
