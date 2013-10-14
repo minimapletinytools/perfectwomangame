c3ad3d7d3e9bf1b291cdb432901fa37c7ea6849b
cmspice <fish@robotbear.org>
2013-10-13 18:30:39 -0700

    added basic stuff into unlock manager with default values.

--- Assets/CODE/SAVE/UnlockManager.cs
+++ Assets/CODE/SAVE/UnlockManager.cs
@@ -4,7 +4,21 @@ using System.Collections;
 [System.Serializable]
 public class Unlockables
 {
-	//TODO	
+	public CharIndexContainerInt unlockedCharacters = new CharIndexContainerInt();
+	public bool skipAvail = false;
+	
+	public Unlockables()
+	{
+		foreach(CharacterIndex e in CharacterIndex.sAllCharacters)
+		{
+			if(e.Choice == 0)
+				unlockedCharacters[e] = 1; //unlocked
+			else if(e.Choice < 4)
+				unlockedCharacters[e] = 2; //hidden
+			else
+				unlockedCharacters[e] = 0; //unknown
+		}
+	}
 }
 
 public class UnlockManager
