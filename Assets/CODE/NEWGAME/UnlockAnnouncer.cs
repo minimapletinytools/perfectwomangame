50e7f6fd99e737da40f5b0381e3386c6210a4826
cmspice <fish@robotbear.org>
2013-10-13 18:01:01 -0700

    added empty UnlockAnnouncer class.

---
+++ Assets/CODE/NEWGAME/UnlockAnnouncer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+//handles all animations when announcing whats been unlocked...
+public class UnlockAnnouncer 
+{
+	
+	NewInterfaceManager NIM {get; set;}
+	public UnlockAnnouncer(NewInterfaceManager aNIM)
+	{
+		NIM = aNIM;
+	}
+	
+	
+	//TODO
+	
+}
