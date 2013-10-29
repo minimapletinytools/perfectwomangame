05ebc1310f1fa0d7b44207e41230ee335bab6707
cmspice <fish@robotbear.org>
2013-10-29 13:29:07 -0700

    som initial work on meta manager and unlock manager.

--- Assets/CODE/MAIN/MetaManager.cs
+++ Assets/CODE/MAIN/MetaManager.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class MetaManager : FakeMonoBehaviour {
 	
 	
+	public UnlockManager UnlockManager {get; private set;}
+	
 	public MetaManager(ManagerManager aManager)
         : base(aManager) 
     {
+		UnlockManager = new UnlockManager();
     }
 	
 	
 	public override void Start () {
-	
+		
 	}
 	
 	
 	public override void Update () {
 	
 	}
+	
 }
--- Assets/CODE/SAVE/UnlockManager.cs
+++ Assets/CODE/SAVE/UnlockManager.cs
@@ -1,5 +1,30 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public static class UnlockRquirements
+{
+	public static Dictionary<CharacterIndex, System.Func<List<PerformanceStats>, string> > 
+		requirements = new Dictionary<CharacterIndex,System.Func<List<PerformanceStats>, string>>()
+	{
+		{ new CharacterIndex(1,1), delegate(List<PerformanceStats> aStats)
+			{
+				return "Playing the game once made you realize you can be star even when really young!";
+			}
+		},{ new CharacterIndex(2,1), delegate(List<PerformanceStats> aStats)
+			{
+				return "";
+			}
+		},{	new CharacterIndex(1,2), delegate(List<PerformanceStats> aStats)
+			{
+				if(aStats.Count() > 7)
+					return "Getting very old made you understand that life should not always be fun and games";
+				return "";
+			}
+		}
+	};
+}
 
 [System.Serializable]
 public class Unlockables
@@ -24,4 +49,25 @@ public class Unlockables
 public class UnlockManager
 {
 	
+	Unlockables mUnlocked;
+	
+	public UnlockManager()
+	{
+		mUnlocked = new Unlockables();
+		//TODO deserialize
+	}
+	
+	public void game_finished(List<PerformanceStats> aStats)
+	{
+		foreach(CharacterIndex e in CharacterIndex.sAllCharacters)
+		{
+			if(mUnlocked.unlockedCharacters[e] != 1)
+				if(UnlockRquirements.requirements.ContainsKey(e))
+				{
+					string msg = UnlockRquirements.requirements[e](aStats);
+					if(msg != "")
+						;//TODO
+				}
+		}
+	}
 }
