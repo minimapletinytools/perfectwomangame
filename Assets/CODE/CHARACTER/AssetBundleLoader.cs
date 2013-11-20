afba20eb824725bac1a45c45b8122c4db912293f
cmspice <fish@robotbear.org>
2013-11-19 18:12:44 -0800

    redid some code in AssetBundleLoader so you can load character with custom callback.

--- Assets/CODE/CHARACTER/AssetBundleLoader.cs
+++ Assets/CODE/CHARACTER/AssetBundleLoader.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 using System.Collections.Generic;
-
+using System;
 public class AssetBundleLoader : FakeMonoBehaviour
 {
  
 
-    public delegate void AssetBundleLoadedDelegate(AssetBundle bundle);
-    System.Collections.Generic.Dictionary<WWW, AssetBundleLoadedDelegate> mRequestLists = new System.Collections.Generic.Dictionary<WWW, AssetBundleLoadedDelegate>();
+    System.Collections.Generic.Dictionary<WWW, Action<AssetBundle> > mRequestLists = new System.Collections.Generic.Dictionary<WWW, Action<AssetBundle>>();
 
     public AssetBundleLoader(ManagerManager aManager)
         : base(aManager) 
@@ -18,7 +17,7 @@ public class AssetBundleLoader : FakeMonoBehaviour
         List<WWW> removal = new System.Collections.Generic.List<WWW>();
         //TODO gets out of sync error here why??
         //not a huge deal since itonly happens in my recording scene. Probbably some stupid threading error...
-        foreach (KeyValuePair<WWW,AssetBundleLoadedDelegate> e in mRequestLists) 
+        foreach (KeyValuePair<WWW,Action<AssetBundle>> e in mRequestLists) 
         {
             if (e.Key.isDone)
             {
@@ -36,12 +35,17 @@ public class AssetBundleLoader : FakeMonoBehaviour
         string filename = Application.dataPath + "/Resources/" + aBundleName + ".unity3d";
         return System.IO.File.Exists(filename);
     }
-
-	public void new_load_character(string aChar, CharacterBundleManager aManager)
+	
+	public void new_load_character(string aChar, CharacterBundleManager aManager, System.Action<AssetBundle> aCallback)
 	{
 		string filename = "file://" + Application.dataPath + "/Resources/" + aChar + ".unity3d";
         //Debug.Log("loading from " + filename);
-        mRequestLists.Add(new WWW(filename), (delegate(AssetBundle aBundle) { aManager.scene_loaded_callback(aBundle,aChar); }));
+        mRequestLists.Add(new WWW(filename), aCallback);
+	}
+	
+	public void new_load_character(string aChar, CharacterBundleManager aManager)
+	{
+		new_load_character(aChar,aManager,delegate(AssetBundle aBundle) { aManager.scene_loaded_callback(aBundle,aChar); });
 	}
 	
 	public void new_load_mini_characater(string aChar, CharacterBundleManager aManager)
