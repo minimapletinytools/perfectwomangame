cd3e1079b5092aed76a7c9110fe75de307dc154c
cmspice <fish@robotbear.org>
2013-11-23 13:34:33 -0800

    setting up unlock announcer...

--- Assets/CODE/NEWGAME/UnlockAnnouncer.cs
+++ Assets/CODE/NEWGAME/UnlockAnnouncer.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 //handles all animations when announcing whats been unlocked...
 public class UnlockAnnouncer 
 {
+	public TimedEventDistributor TED { get; private set; }
+    HashSet<FlatElementBase> mElement = new HashSet<FlatElementBase>();
 	
 	NewInterfaceManager NIM {get; set;}
 	public UnlockAnnouncer(NewInterfaceManager aNIM)
 	{
 		NIM = aNIM;
+		TED = new TimedEventDistributor();
 	}
 	
 	
-	//TODO
+	public void update()
+	{
+		foreach (FlatElementBase e in mElement)
+            e.update(Time.deltaTime);       
+		
+		TED.update(Time.deltaTime);
+	}
+	
+	public void announce_unlock()
+	{
+	}
 	
 }
