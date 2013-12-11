476b68d2c40e9d255caa6ed8effc594528eb1470
cmspice <fish@robotbear.org>
2013-12-10 17:41:37 -0800

    head pop up thingy now basically working.

--- Assets/CODE/NEWGAME/CharacterHeadPopupThingy.cs
+++ Assets/CODE/NEWGAME/CharacterHeadPopupThingy.cs
@@ -20,46 +20,42 @@ public class CharacterHeadPopupThingy
 		
 		TED.update(Time.deltaTime);
 	}
-	
-	FlatElementImage[] mCharacters = null;
-	FlatElementImage[] mBadges = null;
-	
-	public void cleanup()
-	{
-		if(mCharacters != null)
-			for(int i = 0; i < mCharacters.Length; i++)
-				mCharacters[i].destroy();
-		
-		if(mBadges != null)
-			for(int i = 0; i < mBadges.Length; i++)
-				mBadges[i].destroy();
-		
-		mCharacters = null;
-		mBadges = null;
-	}
+
+
 	public void popup_character(CharacterIndex[] aChars, int[] aDiffs)
 	{
 		int count = aChars.Length;
-		
-		cleanup();
+
+		FlatElementImage[] mCharacters = null;
+		FlatElementImage[] mBadges = null;
 		mCharacters = new FlatElementImage[count];
 		mBadges = new FlatElementImage[count];
 
-		Vector3 start = NIM.mFlatCamera.get_point(0,-1.2f);
-		Vector3 step = new Vector3(-400,0,0);
-		Vector3 offset = count*(-step)/2f;
-		Vector3 badgeOffset = new Vector3(-50,-100,0);
 
-		Debug.Log(start);
+		//var sizeImg = ManagerManager.Manager.mCharacterBundleManager.get_image("ICON_05-1");
+		//TODO read from sizeImg
+		float gIconHeight = 343;
+		float gIconWidth = 266;
+
+		Vector3 step = new Vector3(-500,0,0);
+		Vector3 start = NIM.mFlatCamera.get_point(0,-1) - new Vector3(0,gIconHeight/2,0);
+		Vector3 offset = (count-1)*(-step)/2f;
+		Vector3 badgeOffset = new Vector3(-75,150,0);
+
+
 
 		for(int i = 0; i < count; i++)
 		{
-			var img = ManagerManager.Manager.mCharacterBundleManager.get_image("ANGELS_"+aChars[i].StringIdentifier);
+			CharacterBundleManager.ImageSizePair img = null;
+			img = ManagerManager.Manager.mCharacterBundleManager.get_image("ICON_"+aChars[i].StringIdentifier);
+			if(img == null || img.Data == null || img.Image == null)
+				img = ManagerManager.Manager.mCharacterBundleManager.get_image("ANGELS_"+aChars[i].StringIdentifier);
+
 			mCharacters[i] = new FlatElementImage(img.Image,img.Data.Size,10);
 			mCharacters[i].HardPosition = start + offset + step*i;
-			mCharacters[i].SoftPosition = mCharacters[i].SoftPosition + new Vector3(0,300,0);
+			mCharacters[i].SoftPosition = mCharacters[i].SoftPosition + new Vector3(0,gIconHeight + 100,0);
 
-			Debug.Log(mCharacters[i].HardPosition);
+			//Debug.Log(mCharacters[i].HardPosition);
 
 			mBadges[i] = new FlatElementImage(ManagerManager.Manager.mNewRef.bbChoicePerfectIcons[aDiffs[i]],11);
 			mBadges[i].HardColor = GameConstants.UiWhiteTransparent;
@@ -92,7 +88,7 @@ public class CharacterHeadPopupThingy
 					mBadges[workingIndex].Events.add_event(
 						delegate(FlatElementBase aBase, float aTime) 
 						{
-							aBase.mLocalScale = Vector3.one * (1+Mathf.Sin(aTime*6))*1.2f;
+							aBase.mLocalScale = Vector3.one * (1+Mathf.Sin(aTime*6)*0.3f);
 							if(aTime > 0.3f) 
 								return true;
 							return false;
@@ -103,7 +99,7 @@ public class CharacterHeadPopupThingy
 					mCharacters[workingIndex].Events.add_event(
 						delegate(FlatElementBase aBase, float aTime) 
 						{
-							aBase.mLocalScale = Vector3.one * (1+Mathf.Sin(aTime*6))*1.2f;
+							aBase.mLocalScale = Vector3.one * (1+Mathf.Sin(aTime*6)*0.3f);
 							if(aTime > 0.3f) 
 								return true;
 							return false;
@@ -116,13 +112,23 @@ public class CharacterHeadPopupThingy
 			gBadgeTime);
 		}
 
+		//TODO I think this crashes if you skip through th ecutscenes...	
 		//put character back
 		chain = chain.then_one_shot(
 			delegate() {
 				for(int i = 0; i < count; i++)
 				{
+					int index = i;
 					mBadges[i].SoftColor = GameConstants.UiWhiteTransparent;
-					mCharacters[i].SoftPosition = start + offset + step*i;
+					mCharacters[i].SoftPosition = start + offset + step*i + new Vector3(0,-100,0); //move down a little more to compensate for scale change
+					//destroy them eventually.
+					TED.add_one_shot_event(
+						delegate()
+					    {
+							mBadges[index].destroy();
+							mCharacters[index].destroy();
+						},
+					3);
 				}
 			},
 		gTimeAfterBadges);
