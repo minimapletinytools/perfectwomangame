22003bc92300ca090b996df3101755ee72d01062
cmspice <fish@robotbear.org>
2014-03-31 20:12:03 -0700

    simian related scene changes and test stuff

---
+++ Assets/CODE/TESTING/FixedJointChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedJointChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			var joint = GetComponent<HingeJoint>();
+			var limits = joint.limits;
+			limits.min = limits.max = limits.min + 10;
+			joint.limits = limits;
+
+		}
+	}
+}
