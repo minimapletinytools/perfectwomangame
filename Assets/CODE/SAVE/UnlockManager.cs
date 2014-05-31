3a6f1bcdeb2c5d6675e9c513862f0855752aa15d
Peter Lu <chippermonky@gmail.com>
2014-05-30 17:24:07 -0700

    filled out the rest of UnlockManager :).

--- Assets/CODE/SAVE/UnlockManager.cs
+++ Assets/CODE/SAVE/UnlockManager.cs
@@ -50,7 +50,7 @@ public static class UnlockRequirements
             {
                 try{
                     if(aStats.First(e=>e.Character == CharacterIndex.sProf).BadPerformance 
-                        && aStats.First(e=>e.Character == CharacterIndex.sMinister).BadPerformance)
+                       && aStats.First(e=>e.Character == CharacterIndex.sMinister).BadPerformance)
                     {
                         return new UnlockData(){
                             Sentence = "Failing as a Professor and Minister showed you that education should start early!",
@@ -62,6 +62,172 @@ public static class UnlockRequirements
                 catch{
                     return null;
                 }
+            }
+        },{ new CharacterIndex(3,1), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                try{
+                    if(aStats.First(e=>e.Character == CharacterIndex.sProf).BadPerformance)
+                    {
+                        return new UnlockData(){
+                            Sentence = "Failing as a Professor taught you that working hard while young sometimes just doesn't pay off!",
+                            Related = new CharacterIndex[]{CharacterIndex.sProf}
+                        };
+                    }
+                    return null;
+                }
+                catch{
+                    return null;
+                }
+            }
+        },{ new CharacterIndex(3,2), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                try{
+                    if(aStats.First(e=>e.Character == CharacterIndex.sMinister).BadPerformance)
+                    {
+                        return new UnlockData(){
+                            Sentence = "Failing as a Minister showed you the hipocrisy of first world policital solutions",
+                            Related = new CharacterIndex[]{CharacterIndex.sMinister}
+                        };
+                    }
+                    return null;
+                }
+                catch{
+                    return null;
+                }
+            }
+        },{ new CharacterIndex(3,3), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(aHistory.Count > 1 && aHistory.Max(e=>e.Sum(f=>f.Score)) < aStats.Sum(f=>f.Score))
+                {
+                    return new UnlockData(){
+                        Sentence = "Getting a high score showed you the competitive spirit!"
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(4,1), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                return new UnlockData(){
+                    Sentence = "Playing the game once showed you that life doesn't need to be so serious"
+                };
+            }
+        },{ new CharacterIndex(4,2), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(aStats.Count < 5)
+                {
+                    return new UnlockData(){
+                        Sentence = "Dying young showed you such tragedies can happen to YOU"
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(4,3), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(PerformanceStats.history_contains(aStats,new CharacterIndex[]{CharacterIndex.sBeach}))
+                {
+                    return new UnlockData(){
+                        Sentence = "Being so relaxed in your mid thirties reminded you that some people have financial responsibilities at this age :(",
+                        Related = new CharacterIndex[]{CharacterIndex.sBeach}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(5,1), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(PerformanceStats.history_contains(aHistory,new CharacterIndex[]{CharacterIndex.sMother,CharacterIndex.sLeukemia,CharacterIndex.sSister}))
+                {
+                    return new UnlockData(){
+                        Sentence = "Maybe the responsibilty and pain of close family ties just isn't worth it",
+                        Related = new CharacterIndex[]{CharacterIndex.sMother,CharacterIndex.sLeukemia,CharacterIndex.sSister}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(5,2), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(aStats.Where(e=>e.BadPerformance).Count() == aStats.Count)
+                {
+                    return new UnlockData(){
+                        Sentence = "Life is hard"
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(5,3), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(aStats.Last().Character == CharacterIndex.sOneHundred)
+                {
+                    return new UnlockData(){
+                        Sentence = "The beauty of space made you want to experience the beauty of the sea",
+                        Related = new CharacterIndex[]{CharacterIndex.sOneHundred}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(6,1), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(PerformanceStats.history_contains(aStats,new CharacterIndex[]{CharacterIndex.sSlave,CharacterIndex.sSister}))
+                {
+                    return new UnlockData(){
+                        Sentence = "Having been through slavery and war, you want to help the world",
+                        Related = new CharacterIndex[]{CharacterIndex.sSlave,CharacterIndex.sSister}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(6,2), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(PerformanceStats.history_contains(aStats,new CharacterIndex[]{CharacterIndex.sPunk,CharacterIndex.sMother}))
+                {
+                    return new UnlockData(){
+                        Sentence = "Sometimes kids just don't behave!",
+                        Related = new CharacterIndex[]{CharacterIndex.sPunk,CharacterIndex.sMother}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(6,3), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(PerformanceStats.history_contains(aStats,new CharacterIndex[]{CharacterIndex.sSexy,CharacterIndex.sPunk,CharacterIndex.sFundraiser}))
+                {
+                    return new UnlockData(){
+                        Sentence = "Using your physical charisma so much throughout your life showed you the power of sex appeal",
+                        Related = new CharacterIndex[]{CharacterIndex.sSexy,CharacterIndex.sPunk,CharacterIndex.sFundraiser}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(7,1), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(aHistory.Count > 10)
+                {
+                    return new UnlockData(){
+                        Sentence = "Having experienced 10 different lives makes you realize the possibilities in life can be a little overwhelming."
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(7,2), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(PerformanceStats.history_contains(aStats,new CharacterIndex[]{CharacterIndex.sDemented}))
+                {
+                    return new UnlockData(){
+                        Sentence = "Having being a demented old lady showed you the importance of faith in old age",
+                        Related = new CharacterIndex[]{CharacterIndex.sDemented}
+                    };
+                }
+                return null;
+            }
+        },{ new CharacterIndex(7,3), delegate(List<PerformanceStats> aStats, List<List<PerformanceStats> > aHistory)
+            {
+                if(aStats.Last().Character == CharacterIndex.sPray && aStats.Last().DeathTime != -1)
+                {
+                    return new UnlockData(){
+                        Sentence = "Death while praying expanded your spiritual possibilities",
+                        Related = new CharacterIndex[]{CharacterIndex.sPray}
+                    };
+                }
+                return null;
             }
         }
 	};
