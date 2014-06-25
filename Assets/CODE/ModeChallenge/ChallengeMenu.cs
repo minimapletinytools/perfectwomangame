7e809973848ab1428c57de61d75970b13dc034e5
Peter Lu <chippermonky@gmail.com>
2014-06-25 11:46:05 -0700

    made some skeletons for ChallengeMode.

---
+++ Assets/CODE/ModeChallenge/ChallengeMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChallengeMenu 
+{
+    enum ChoiceMode{
+        NONE,
+        AGE,
+        CHAR,
+        DIFF,
+        MODE
+    }
+
+    ChoiceMode mMode = ChoiceMode.NONE;
+    ModeChallenge mManager;
+    ChoiceHelper mChoiceHelper;
+
+    public ChallengeMenu(ModeChallenge aManager)
+    {
+        mManager = aManager;
+    }
+
+    public void initialize()
+    {
+        mChoiceHelper = new ChoiceHelper();
+    }
+
+    public void set_age_select()
+    {
+        mMode = ChoiceMode.AGE;
+    }
+
+    public void set_char_select()
+    {
+        mMode = ChoiceMode.CHAR;
+    }
+}
--- Assets/CODE/ModePlay/CHOICES/SetChoiceInterface.cs
+++ Assets/CODE/ModePlay/CHOICES/SetChoiceInterface.cs
@@ -23,3 +23,22 @@ public class SetPlayChoice : SetChoiceInterface
 		CM.set_bb_choice_percentages(aChoice,aPercentage);
 	}
 }
+
+public class SetChallengeChoice : SetChoiceInterface
+{
+    ChallengeMenu CM {get; set;}
+    public SetChallengeChoice(ChallengeMenu aMenu)
+    {
+        CM = aMenu;
+    }
+    public void set_choice(int aChoice)
+    {
+        //TODO
+        //CM.set_bb_choice(aChoice);
+    }
+    public void set_choice_percentages(int aChoice, float aPercentage)
+    {
+        //TODO
+        //CM.set_bb_choice_percentages(aChoice,aPercentage);
+    }
+}
