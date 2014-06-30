49bb426014a2c741ee5971c31db8d904e857c583
Peter Lu <chippermonky@gmail.com>
2014-06-30 08:52:00 -0700

    added test script

---
+++ Assets/CODE/TESTING/RandomRotater.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+
+    }
+}
