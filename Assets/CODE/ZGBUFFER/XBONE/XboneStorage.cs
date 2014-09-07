4f204a25628d64104ea0a2e6d7cbc24448876a4b
Peter Lu <chippermonky@gmail.com>
2014-09-07 11:32:28 -0700

    minor

--- Assets/CODE/ZGBUFFER/XBONE/XboneStorage.cs
+++ Assets/CODE/ZGBUFFER/XBONE/XboneStorage.cs
@@ -3,21 +3,36 @@ using System.Collections;
 
 public class XboneStorage 
 {
+
+#if UNITY_XBOXONE 
 	public void Start()
 	{
 		Storage.StorageManager.Create();
-		
-		
-		//Storage.StorageManager.AmFullyInitialized ();
-		//note this creates a ConnectedStorage Ojbect, container string is legacy, it's the 'active container'
-		//TODO add callbakc
-		//var createStorageOp = Storage.ConnectedStorage.CreateAsync (Users.UsersManager.Inst.Users [0].Id, "main_save", null);
-		//if (createStorageOp.IsComplete) { //actually just do this in the callback instead
-		//var container = createStorageOp.Storage.OpenOrCreateContainer ("main_save");
-		//TODO
-		//container.ReadAsync
-		//TODO update the Datamap in the container...
-		//}
+		//TODO create thread
+
 
 	}
+
+	void save_thread()
+	{
+		if(Storage.StorageManager.AmFullyInitialized ())
+		{
+			//note this creates a ConnectedStorage Ojbect, container string is legacy, it's the 'active container'
+			//TODO add callbakc
+			//var createStorageOp = Storage.ConnectedStorage.CreateAsync (Users.UsersManager.Inst.Users [0].Id, "main_save", null);
+			//if (createStorageOp.IsComplete) { //actually just do this in the callback instead
+			//var container = createStorageOp.Storage.OpenOrCreateContainer ("main_save");
+			//TODO
+			//container.ReadAsync
+			//TODO update the Datamap in the container...
+			//}
+		}
+	}
+
+	
+#else
+	public void Start()
+	{
+	}
+#endif
 }
